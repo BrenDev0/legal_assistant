fa5bd490ea13f79f00ec989ed5a46c663dfce5ea
BrenDev0 <lahey1991@gmail.com>
2025-08-25 16:46:53 -0600

    leagl researcher dependencies

--- src/dependencies/configure_container.py
+++ src/dependencies/configure_container.py
@@ -3,6 +3,7 @@ from src.api.core.services.redis_service import RedisService
 from src.workflow.services.embeddings_service import EmbeddingService
 from src.workflow.services.prompt_service import PromptService
 from src.workflow.agents.context_orchestrator.context_orchestrator_agent import ContextOrchestrator
+from src.workflow.agents.general_legal_research.general_legal_agent import GeneralLegalResearcher
 from src.workflow.services.llm_service import LlmService
 from src.api.modules.interactions.interactions_dependencies import configure_interactions_dependencies
 
@@ -28,13 +29,17 @@ def configure_container():
   )
   Container.register("prompt_service", prompt_service)
  
- 
+ ## Agents ##
   context_orchestrator_agent = ContextOrchestrator(
     prompt_service=prompt_service,
     llm_service=llm_service
   )
   Container.register("context_orchestrator_agent", context_orchestrator_agent)
   
-  
+  general_legal_researcher = GeneralLegalResearcher(
+    prompt_service=prompt_service,
+    llm_service=llm_service
+  )
+  Container.register("general_legal_researcher", general_legal_researcher)
   ## Modules # All core dependencies must be configured above this line ##
   configure_interactions_dependencies()
