a64be1a68f8633690aa43d81c926655ee5ca7244
BrenDev0 <lahey1991@gmail.com>
2025-09-26 10:57:46 -0600

    added route to check websoket connections

--- src/api/server.py
+++ src/api/server.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from fastapi.middleware.cors import CORSMiddleware
-from src.dependencies.configure_container import configure_container
-from src.api.modules.interactions import interactions_routes, interactions_ws
 
+from src.dependencies.configure_container import configure_container
+from src.dependencies.services import get_websocket_service
+
+from src.api.modules.interactions import interactions_routes, interactions_ws
+from src.api.modules.websocket.websocket_service import WebsocketService
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -21,5 +24,24 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+@app.get("/health", tags=["Internal"])
+async def health():
+    """
+    ## Health check 
+    This endpoints verifies server status.
+    """
+    return {"status": "ok"}
+
 app.include_router(interactions_routes.router)
 app.include_router(interactions_ws.router)
+
+@app.get("/connections", tags=["Internal"])
+async def get_websocket_connections():
+    service = get_websocket_service()
+
+    connections = service.active_connections
+
+    return {
+        "connection_ids": list(connections.keys()),
+        "count": len(connections)
+    }
