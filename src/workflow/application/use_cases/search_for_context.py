b8f600aca46e4cf064a4f39f765eac8fa746e07d
BrenDev0 <lahey1991@gmail.com>
2025-10-15 14:11:08 -0600

    search results

--- src/workflow/application/use_cases/search_for_context.py
+++ src/workflow/application/use_cases/search_for_context.py
@@ -16,12 +16,13 @@ class SearchForContext():
     async def execute(
         self,
         input: str,
-        namespace: str
+        namespace: str,
+        top_k: int = 2
     ) -> str:
         query_vector = await self.__embedding_service.embed_query(input)
         results = await self.__repository.similarity_search(
             query_vector=query_vector,
-            top_k=5,
+            top_k=top_k,
             namespace=namespace
         )
 
