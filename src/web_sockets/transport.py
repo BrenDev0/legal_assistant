e3e0145072616b89da82ac4f65296012e0b44776
BrenDev0 <lahey1991@gmail.com>
2025-11-26 11:38:17 -0600

    ws cleanup

--- src/web_sockets/transport.py
+++ src/web_sockets/transport.py
@@ -1,7 +1,7 @@
-import logging
-from websockets import ConnectionClosed
 from typing import Union, Any
 from uuid import UUID
+import logging
+
 from src.web_sockets.connections import WebsocketConnectionsContainer
 logger = logging.getLogger(__name__)
 
@@ -11,15 +11,16 @@ class WebSocketTransportService:
         connection_id: Union[str, UUID],
         data: Any
     ):
-        ws = WebsocketConnectionsContainer.resolve_connection(connection_id=connection_id)
+        ws = WebsocketConnectionsContainer.resolve_connection(connection_id=str(connection_id))
 
         if ws:
             try: 
                 await ws.send_json(data)
-            
-            except ConnectionClosed:
-                logger.info(f"Connection {connection_id} disconnected")
-
+                
             except Exception as e:
-                logger.info(f"Connection id: {connection_id}::::, Error sending data:::: {e}")
+                if "closed" in str(e).lower() or "disconnect" in str(e).lower():
+                    logger.info(f"Connection {connection_id} disconnected")
+                    return
+                
+                logger.error(f"Connection id: {connection_id}::::, Error sending data:::: {e}")
                 raise e
